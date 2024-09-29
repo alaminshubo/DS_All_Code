ea3d0428f98a30ae0cdd2900ed769721634df615
Md. Al Amin <168575309+alaminshubo@users.noreply.github.com>
2024-09-29 21:28:05 +0600

    Add files via upload

---
+++ Backward_Decremental.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int quiz[5] = {5,15,20,10,7};
+    for(int i = 4; i >= 0; i--){
+        cout << quiz[i] << " ";
+    }
+    return 0;
+}
+
