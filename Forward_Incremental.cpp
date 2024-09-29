99d550711a1f43f92e1e983076e4b6524a2a8c6e
Md. Al Amin <168575309+alaminshubo@users.noreply.github.com>
2024-09-29 21:27:34 +0600

    Add files via upload

---
+++ Forward_Incremental.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int quiz[5] = {5,15,20,10,7};
+    for(int i = 0; i <= 4; i++){
+        cout << quiz[i] << " ";
+    }
+    return 0;
+}
